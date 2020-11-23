6032f4418188a0ab662c5b5df4466b499a37447d
MrHe-Husky <55549940+MrHe-Husky@users.noreply.github.com>
2020-11-23 20:16:43 +0800

    today's leecode

---
+++ LeeCodeQuestions/FlattenMultilevelDoublyLinkedList430.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlattenMultilevelDoublyLinkedList430
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+          }
+     }
+
+     public class Solution
+     {
+          public Node Flatten(Node head)
+          {
+               if (head == null)
+               {
+                    return head;
+               }
+               Stack<Node> branchNode = new Stack<Node>();
+               Node PNode = head;
+               Node tempNode;
+               while (!(PNode.next == null && branchNode.Count == 0&&PNode.child==null)) 
+               {
+                    //有孩子节点
+                    if (PNode.child!=null)
+                    {
+                         if (PNode.next!=null)
+                         {
+                              branchNode.Push(PNode.next);
+                         }
+                         tempNode = PNode;
+                         PNode.next = PNode.child;
+                         PNode = PNode.next;
+                         PNode.prev = tempNode;
+                         tempNode.child = null;
+                         continue;
+                    }
+                    //下一个节点为空，要跳回上一段链表
+                    if (PNode.next==null&&branchNode.Count!=0)
+                    {
+                         tempNode = PNode;
+                         PNode.next = branchNode.Pop();
+                         PNode = PNode.next;
+                         PNode.prev = tempNode;
+                         continue;
+                    }
+                    //非以上情况，在本链表中继续
+                    if (PNode.next!=null)
+                    {
+                         PNode = PNode.next;
+                         continue;
+                    }
+               }
+               return head;
+          }
+     }
+
+     public class Node
+     {
+          public int val;
+          public Node prev;
+          public Node next;
+          public Node child;
+     }
+}
---
+++ LeeCodeQuestions/GenerateRandomPointInCircle478.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerateRandomPointInCircle478
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               Random rd = new Random();
+               double radius, x_center, y_center;
+               radius = 8;
+               x_center = 2;
+               y_center = 7;
+               Solution obj = new Solution(radius, x_center, y_center);
+               double[] param_1 = obj.RandPoint();
+               Console.WriteLine($"随机点X：{param_1[0]},随机点Y：{param_1[1]}");
+               bool Successed = false;
+               Successed = (Math.Pow((param_1[0] - x_center), 2) + Math.Pow((param_1[1] - y_center), 2)) < (radius * radius);
+               Console.WriteLine(Successed);
+               Console.ReadKey();
+          }
+     }
+
+     public class Solution
+     {
+          double radius;
+          double centerX;
+          double centerY;
+          public Solution(double radius, double x_center, double y_center)
+          {
+               this.radius = radius;
+               this.centerX = x_center;
+               this.centerY = y_center;
+          }
+
+          public double[] RandPoint()
+          {
+               double[] param = new double[] { -1, -1 };
+               double tempX, tempY;
+               Random rd = new Random();
+               do
+               {
+                    tempX = centerX + (2 * rd.NextDouble() - 1) * radius;
+                    tempY = centerY + (2 * rd.NextDouble() - 1) * radius;
+               } while ((param[0]-centerX)*(param[0] - centerX) + (param[1] - centerY)*(param[1] - centerY) < (radius * radius));
+               param[0] = tempX;
+               param[1] = tempY;
+               return param;
+          }
+     }
+     /*
+      public class Solution
+     {
+          double radius;
+          double centerX;
+          double centerY;
+          public Solution(double radius, double x_center, double y_center)
+          {
+               this.radius = radius;
+               this.centerX = x_center;
+               this.centerY = y_center;
+          }
+
+          public double[] RandPoint()
+          {
+               double[] param = new double[] { -1, -1 };
+               double tempX, tempY;
+               Random rd = new Random();
+               tempX = centerX + (2 * rd.NextDouble() - 1) * radius;
+               tempY = centerY + (2 * rd.NextDouble() - 1) * Math.Sqrt(radius * radius - (tempX-centerX) * (tempX-centerX));
+               param[0] = tempX;
+               param[1] = tempY;
+               return param;
+          }
+     }
+     */
+}
