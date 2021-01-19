e7d73534a7b679260beae3b2cd48d0a516ef4376
MrHe-Husky <55549940+MrHe-Husky@users.noreply.github.com>
2021-01-19 21:39:35 +0800

    Create TravelEnd1436.cs

---
+++ LeeCodeQuestions/TravelEnd1436.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelEnd1436
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+          }
+
+          public class Solution
+          {
+               public string DestCity(IList<IList<string>> paths)
+               {
+                    bool endCityFlag = false;
+                    foreach (var path in paths)
+                    {
+                         string TempCity = path[1];
+                         endCityFlag = true;
+                         foreach (var path1 in paths)
+                         {
+                              if (path1[0]==TempCity)
+                              {
+                                   endCityFlag = false;
+                                   break;
+                              }
+                         }
+                         if (endCityFlag)
+                         {
+                              return TempCity;
+                         }
+                    }
+                    return null;
+               }
+          }
+     }
+}
