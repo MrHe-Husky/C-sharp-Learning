62d40cf666ce6b3e4b4fbd261034d85840008b5d
MrHe-Husky <55549940+MrHe-Husky@users.noreply.github.com>
2021-01-20 00:53:57 +0800

    Create TwoSum1.cs

---
+++ LeeCodeQuestions/TwoSum1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace TwoSum1
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               int[] nums = { 2, 3, 4 };
+               int target = 6;
+               Solution solution = new Solution();
+               int[] backarray = solution.TwoSum(nums, target);
+               Console.Write("{0} {1}", backarray[0], backarray[1]);
+          }
+     }
+
+     //存在bug，当输入数组内存在两个相同数字时可能会有已存在键的情况。此时不可使用哈希表方法
+     public class Solution
+     {
+          public int[] TwoSum(int[] nums, int target)
+          {
+               var hashtable = new Hashtable();
+               for (int i = 0; i < nums.Length; i++)
+               {
+                    if (hashtable.ContainsKey(target - nums[i]))
+                    {
+                         return new int[] { (int)hashtable[target - nums[i]], i };
+                    }
+                    hashtable.Add(nums[i], i);
+               }
+               return new int[] { -1, -1 };
+          }
+     }
+}
