58ac1a1d96be07ba5017f8f13c9f38c07a1525e6
MrHe-Husky <55549940+MrHe-Husky@users.noreply.github.com>
2021-01-22 23:02:56 +0800

    Create ClassAndInstance.cs

---
+++ Class/ClassAndInstance.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassAndInstance
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               double x = Math.Pow(114.514, 8.10);
+               Console.WriteLine(x);
+
+               /*用new由类建立实例
+               (new Form()).ShowDialog();
+               */
+               Form myForm;
+               myForm = new Form();
+               myForm.Text = "My Form";//更改属性text的名字
+               myForm.ShowDialog();
+          }
+     }
+}
