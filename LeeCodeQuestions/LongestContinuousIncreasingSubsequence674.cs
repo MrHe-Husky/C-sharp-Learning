10e97297dbee2730f283845029f8a70670486af7
MrHe-Husky <55549940+MrHe-Husky@users.noreply.github.com>
2021-01-24 21:25:35 +0800

    Create LongestContinuousIncreasingSubsequence674.cs

---
+++ LeeCodeQuestions/LongestContinuousIncreasingSubsequence674.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestContinuousIncreasingSubsequence674
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               int[] numArray = {1,3,5,7 };
+               Console.WriteLine("{0}",new Solution().FindLengthOfLCIS(numArray));
+          }
+     }
+
+     public class Solution
+     {
+          public int FindLengthOfLCIS(int[] nums)
+          {
+               int longestQueue = 0;
+               int tempIncreasingQueue = 0;
+               int preNum = int.MinValue;
+               for (int i = 0; i < nums.Length; i++)
+               {
+                    if (nums[i]>preNum)
+                    {
+                         tempIncreasingQueue++;
+                         preNum = nums[i];
+                    }
+                    else
+                    {
+                         if (tempIncreasingQueue>longestQueue)
+                         {
+                              longestQueue = tempIncreasingQueue;
+                         }
+                         tempIncreasingQueue = 1;
+                         preNum = nums[i];
+                    }
+               }
+               if (tempIncreasingQueue>longestQueue)
+               {
+                    longestQueue = tempIncreasingQueue;
+               }
+               return longestQueue;
+          }
+     }
+}
