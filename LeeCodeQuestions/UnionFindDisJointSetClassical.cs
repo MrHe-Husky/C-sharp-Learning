d0afa29d48714703cb6dd1e7968edf9cba27b500
MrHe-Husky <55549940+MrHe-Husky@users.noreply.github.com>
2021-01-27 20:13:30 +0800

    Update UnionFindDisJointSetClassical.cs

--- LeeCodeQuestions/UnionFindDisJointSetClassical.cs
+++ LeeCodeQuestions/UnionFindDisJointSetClassical.cs
@@ -11,7 +11,7 @@ namespace UnionFindDisJointSetClassical
           static void Main(string[] args)
           {
                //测试用例1:如果有回路则输出"";无回路输出"";
-               int[,] paths = new int[6, 2] { { 1, 2 }, { 3, 6 }, { 2, 3 }, { 2, 4 }, { 4, 5 }, { 3, 4 } };
+               int[,] paths = new int[6, 2] { { 1, 2 }, { 3, 6 }, { 2, 3 }, { 2, 4 }, { 4, 5 }, { 3, 4 } };     ////此例存在回路
                //int[,] paths = new int[5, 2] { { 1, 2 }, { 3, 6 }, { 2, 3 }, { 2, 4 }, { 4, 5 } };               ////此例不存在回路
                var disjointset = new UnionFindDisjoinSets();
                bool existFlag = false;
@@ -41,18 +41,18 @@ namespace UnionFindDisJointSetClassical
      /// 作用:在一个无向图中检查是否出现了环
      /// Version2目标改进方面:路径压缩、能够保存所有集合的根节点。
      /// </summary>
-     class UnionFindDisjoinSets
+     class UnionFindDisjoinSets_v1
      {
           Dictionary<int,int> disjointSets = new Dictionary<int,int>();
 
           //查找根节点函数,查
           public int FindRoot(int point)
           {
-               if (!disjointSets.ContainsKey(point))
+               if (!disjointSets.ContainsKey(point))//找不到它的上一节点(此节点即为根节点)
                {
                     return point;
                }
-               while (disjointSets.ContainsKey(point))
+               while (disjointSets.ContainsKey(point))//存在上级节点(此点非根节点)，循环寻找直到根节点
                {
                     point = disjointSets[point];
                }
@@ -65,6 +65,49 @@ namespace UnionFindDisJointSetClassical
                int pointARoot = FindRoot(pointA);
                int pointBRoot = FindRoot(pointB);
                if (pointARoot==pointBRoot)
+               {
+                    return false;
+               }
+               disjointSets[pointB] = pointARoot;
+               return true;
+          }
+     }
+
+     /// <summary>
+     /// Version2:
+     /// 使用数组来做存储树的结构;输入格式为两个点{x,y};
+     /// 作用:在一个无向图中检查是否出现了环
+     /// Version2改进方面:路径压缩、能够保存所有集合的根节点。
+     /// Version3目标:能够根据根节点找到所有同集合的点。
+     /// </summary>
+     class UnionFindDisjoinSets
+     {
+          Dictionary<int, int> disjointSets = new Dictionary<int, int>();
+          HashSet<int> rootPointsSet = new HashSet<int>();
+
+          //查找根节点函数,查
+          public int FindRoot(int point)
+          {
+               if (!disjointSets.ContainsKey(point))//找不到它的上一节点(此节点即为根节点)
+               {
+                    rootPointsSet.Add(point);
+                    return point;
+               }
+               int rootPoint = point;
+               while (disjointSets.ContainsKey(rootPoint))//存在上级节点(此点非根节点)，循环寻找直到根节点
+               {
+                    rootPoint = disjointSets[rootPoint];
+               }
+               disjointSets[point] = rootPoint;//指向根节点，降低秩，加快找到根的速度。
+               return rootPoint;
+          }
+
+          //将连线AB加入，并
+          public bool UnionElement(int pointA, int pointB)
+          {
+               int pointARoot = FindRoot(pointA);
+               int pointBRoot = FindRoot(pointB);
+               if (pointARoot == pointBRoot)
                {
                     return false;
                }
